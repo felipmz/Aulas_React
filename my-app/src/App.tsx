c97f01c843e4cecd057beb849476cbd11c60bd27
felipmz <felipemenezzees@gmail.com>
2025-07-09 07:46:15 -0300

    .

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,13 +6,16 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-
+  const url:string = "https//:youtube.com";
 
 
   return (
     <>
+    
+
       <div className="card">
           <h1>Ola mundo em react</h1>
+          <img src={url} alt="imagem" />
       </div>
     </>
   )
