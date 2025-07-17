b00d1cc44bbb7c9d1b5ed7e9a952e3ab2cdc99b4
felipmz <felipemenezzees@gmail.com>
2025-07-17 16:47:41 -0300

    estilos css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,14 +2,20 @@ import './App.css'
 
 import Coisa from './componentes/Coisa'
 import PropsTeste from './componentes/PropsTeste'
-
+import styles from './componentes/compoCSS.module.css'
+//é possivel criar os componentes CSS em um arquivo separado e os importar para os elemwntos
 function App() {
 
   return (
     <>
-      <div className="card">
+      <div className={styles.letras} >
           <h1>Ola mundo em react</h1>
-          <Coisa/>
+
+         
+          <div className={styles.ParaParagrafos}>
+            <Coisa />
+          </div>
+          
           <PropsTeste nome={"felipe"} idade={12}/>
       </div>
     </>
